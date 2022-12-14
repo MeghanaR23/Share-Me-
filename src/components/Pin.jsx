2f2b3fce96e6bb5d432456b8dc657a1c26708926
Meghana <meghana.monica@gmail.com>
2022-12-14 19:05:00 +0530

    Removed On Hovered,added Image

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOffline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
 import { client, urlFor } from '../client';
 import { fetchUser } from '../utils/fetchUser';
+import noImage from '../assets/no_image.png'
 
 const Pin = ({ pin: {postedBy, image, _id, save }}) => {
-    const [postHovered, setPostHovered]=useState(false);
     const navigate = useNavigate();
     const user = fetchUser();
     const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
@@ -40,17 +40,14 @@ const Pin = ({ pin: {postedBy, image, _id, save }}) => {
         });
     };
   return (
-    <div className="m-2">
+    <div className="m-2 relative cursor-pointer group">
         <div
-        onMouseEnter={() => setPostHovered(true)}
-        onMouseLeave={() => setPostHovered(false)}
         onClick={ () => navigate(`/pin-detail/${_id}`)}
         classname="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
         >
-        <img className="rounded-lg w-full" alt="user-post" src={urlFor(image).width(250).url()}/>
-        {postHovered && (
+        <img className="rounded-lg w-full" alt="user-post" src={image ? urlFor(image).width(250).url() : noImage}/>
           <div 
-            className ="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            className ="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50 hidden group-hover:block"
             style={{ height: '100%' }}
           >
             <div className = "flex items-center justify-between">
@@ -102,12 +99,11 @@ const Pin = ({ pin: {postedBy, image, _id, save }}) => {
               </div>   
            </div>
          </div>
-            )}
        </div>
        <Link to={`user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
         <img
         className="w-8 h-8 rounded-full object-cover" 
-        src={postedBy?.image}
+        src={postedBy.image ?? noImage}
         alt="user-profile"/>
         <p className="font-semibold capitalize">{postedBy?.userName}</p>
        </Link>
